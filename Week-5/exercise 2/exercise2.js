26ec541214c571e35942512dcc03a0e76c7e4fa4
hkyakka <114503739+hkyakka@users.noreply.github.com>
2022-11-06 23:35:38 +0530

    Corrected Exercise 2
    Vowel checker added to the code.

--- Week-5/exercise 2/exercise2.js
+++ Week-5/exercise 2/exercise2.js
@@ -1,7 +1,11 @@
-function vowelCount(str){
+const vowels = ['a','e','i','o','u']
+const vowelCount = (str) =>{
     let map = new Map();
-    str.split("").map(s=>{ s=s.toLowerCase();map.has(s) ? map.set(s,(map.get(s) + 1)) : map.set(s,1)})
+    [...str.toLowerCase()].forEach( s => {
+        vowels.includes(s) ? map.has(s) ? map.set(s,(map.get(s) + 1)) : map.set(s,1):map
+    });
     return map;
 }
 
 console.log(vowelCount("Hello World"))
+console.log(vowelCount("This is to check Vowles and Count."))
